3af7b2e1b5f1843af47c22b4868ce3bf438ebb92
viivi <viivi.kerri@gmail.com>
2012-10-10 20:27:26 +0300

    muutos

--- src/view/CourseVisualizationApplet.java
+++ src/view/CourseVisualizationApplet.java
@@ -12,10 +12,19 @@ public class CourseVisualizationApplet extends PApplet {
 	@Override
 	public void draw() {
 		this.background(255);
+		this.drawCircle();
 	}
 
 	@Override
 	public void setup() {
 		this.size(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		this.size(800, 600);
+	}
+	
+	void drawCircle() {
+		
+		arc(400, 300, 250, 250, 0, HALF_PI);
+		fill(22, 220, 230);
+		
 	}
 }
