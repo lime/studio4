80f18418a1832d61f48207fde62f7ab99b64c7c9
Petri Uotila <petri.uotila@aalto.fi>
2012-10-10 19:31:13 +0300

    Ikkunan koko + inforectangle

--- src/view/CourseVisualizationApplet.java
+++ src/view/CourseVisualizationApplet.java
@@ -12,10 +12,12 @@ public class CourseVisualizationApplet extends PApplet {
 	@Override
 	public void draw() {
 		this.background(255);
+		rect(598, 50, 200, 500, 20, 20);
 	}
+	
 
 	@Override
 	public void setup() {
-		this.size(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		this.size(800, 600);
 	}
 }
