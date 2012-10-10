71ea755759b54e6bf7ebdd046650c1cadb7d938c
Emil S <emil.sagfors@aalto.fi>
2012-10-10 18:20:17 +0300

    initialized empty Processing applet

---
+++ src/view/CourseVisualizationApplet.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package view;
+
+import processing.core.PApplet;
+
+/**
+ *
+ */
+public class CourseVisualizationApplet extends PApplet {
+	@Override
+	public void draw() {
+		this.background(255);
+	}
+
+	@Override
+	public void setup() {
+		this.size(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+}
