361d83096256a89a2619f3ae9f844fc52463e80f
Emil S <emil.sagfors@aalto.fi>
2012-10-12 12:32:15 +0300

    print title text of selected node

--- src/view/CourseVisualizationApplet.java
+++ src/view/CourseVisualizationApplet.java
@@ -14,52 +14,70 @@ import processing.core.PApplet;
  */
 @SuppressWarnings("serial")
 public class CourseVisualizationApplet extends PApplet {
-	
+
 	private static final String DATA_FILENAME = "data/kurssit.xml";
-	
+
 	private CourseHierarchyNode courseData;
-	
+	private CourseHierarchyNode selectedNode;
+
 	@Override
 	public void draw() {
 
 		this.background(255);
-		rect(598, 50, 200, 500, 20, 20);
+
+		// draw infobox
 		this.fill(22, 220, 230);
+		this.drawInfobox(selectedNode);
+
 		this.drawCircle(courseData);
-		
+
 		this.drawCircle(courseData);
 	}
-	
+
+	private void drawInfobox(CourseHierarchyNode displayNode) {
+		// save the current matrix state
+		this.pushMatrix();
+		this.translate(598, 50);
+
+		this.rect(0, 0, 200, 500, 20, 20);
+
+		String title = displayNode.getTitle();
+		if (title == null) {
+			title = "Untitled";
+		}
+		this.fill(255);
+		this.text(title, 10, 10);
+		// reset matrix
+		this.popMatrix();
+	}
 
 	private void drawCircles(CourseHierarchyNode data) {
 	}
-	
-
 
 	void drawCircle(CourseHierarchyNode parentNode) {
-		
-		float edellinenKulma=0;
+
+		float edellinenKulma = 0;
 		List<? extends CourseHierarchyNode> children = parentNode.getChildren();
-		for(int i=0; i<children.size(); i++) {
-		
-		float kulma = 2*PI*children.get(i).getWeight()/parentNode.getWeight();
-		this.arc(400, 300, 250, 250
-				, edellinenKulma, kulma);
-		fill(22, 220, 230);
-		
-		edellinenKulma = 2*PI*children.get(i).getWeight()/parentNode.getWeight();
-		
-		System.out.println("CourseVisualizationApplet.drawCircle() "+children);
+		for (int i = 0; i < children.size(); i++) {
+
+			float kulma = 2 * PI * children.get(i).getWeight()
+					/ parentNode.getWeight();
+			this.arc(400, 300, 250, 250, edellinenKulma, kulma);
+			fill(22, 220, 230);
+
+			edellinenKulma = 2 * PI * children.get(i).getWeight()
+					/ parentNode.getWeight();
+
 		}
-		
-		
+
 	}
 
 	@Override
 	public void setup() {
 		this.size(800, 600);
-		
+
 		this.courseData = JAXBParser.getCourseHierarchy(DATA_FILENAME);
+		this.selectedNode = this.courseData;
 		this.size(800, 600);
 	}
 }
